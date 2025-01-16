4b0793b1eb1f4488b2cd937edbc9e849873666f7
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2025-01-16 13:56:52 +0100

    feat: add TimeUtils for date formatting and enhance WEForumScrapper with new methods

--- src/data/scrapping/weforum.py
+++ src/data/scrapping/weforum.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from utils.env_loader import EnvLoader
+from utils.time import TimeUtils
 
 
 class WEForumScrapper:
@@ -11,10 +12,21 @@ class WEForumScrapper:
         self.weforum_email = EnvLoader.weforum_email
         self.weforum_passwd = EnvLoader.weforum_passwd
         self.timeout = EnvLoader.webdriverwait_timeout
+        self.cybersecturity_topic_url = (
+            "https://intelligence.weforum.org/topics/a1Gb00000015LbsEAE"
+        )
+        self.home_page = "https://www.weforum.org/"
 
     def _login(self):
+        """Log in to the World Economic Forum website using the credentials provided in the .env file
+
+        Raises:
+            TimeoutError: If the login button is not found after 5 seconds
+        """
+
+        # TODO: Implement login within the original url if possible and available
         original_url = self.driver.current_url
-        self.driver.get("https://www.weforum.org/")
+        self.driver.get(self.home_page)
         sign_in_bttn = self.driver.getElementById("navbar_sign_in")
         sign_in_bttn.click()
 
@@ -46,6 +58,23 @@ class WEForumScrapper:
 
         # Return to the original page
         self.driver.get(original_url)
+
+    def _get_links_to_scrape(self, max_age_date: int = 7) -> list:
+        """Get the links to the articles to scrape
+
+        Args:
+            max_age_dates (int): List of dates to filter the articles
+
+        Returns:
+            list: List of links to the articles to scrape
+        """
+
+        if max_age_date < 1:
+            raise ValueError("max_age_date must be greater than 0")
+
+        age_words = TimeUtils.age_format_date(max_age_date)
+
+        # TODO: continue
 
     def close(self):
         self.driver.close()
