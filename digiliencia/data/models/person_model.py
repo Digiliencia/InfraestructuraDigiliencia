2acb3aac04cac2c48475c73715b0fd1d675f059d
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2025-03-28 13:48:25 +0100

    feat: add data models for news, organization, person, and topic

---
+++ digiliencia/data/models/person_model.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from digiliencia.data.models.interface_model import IModel
+
+
+@dataclass(frozen=True)
+class PersonModel(IModel):
+    """Represents a person in the database."""
+
+    id: int
+    full_name: str
+    email: str
+    decription: str
