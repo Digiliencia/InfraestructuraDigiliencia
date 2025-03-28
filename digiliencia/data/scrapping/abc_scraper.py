57d60cd30bc666602943280061f07e2bc628cf59
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2025-03-28 13:51:46 +0100

    feat: update AbstractScraper to return ScrapedNewsModel in scrap method

--- digiliencia/data/scrapping/abc_scraper.py
+++ digiliencia/data/scrapping/abc_scraper.py
@@ -1,25 +1,27 @@
 from abc import ABC, abstractmethod
+from digiliencia.data.models.news_model import ScrapedNewsModel
+
 
 class AbstractScraper(ABC):
     """
     Abstract base class for all scrapers
-    
+
     This class defines the interface for scrapers.
     """
-    
+
     @abstractmethod
-    def scrap(self, from_days_ago: int) -> tuple[dict[str, str]]:
+    def scrap(self, from_days_ago: int) -> tuple[ScrapedNewsModel]:
         """
         Scraps the class content from from_days_ago days ago.
-        
+
         Parameters
         ----------
         from_days_ago : int
             The number of days ago from which to start the scraping.
-        
+
         Returns
         -------
-        tuple[dict[str, str]]
-            A tuple containing the scraped data.
+        tuple[ScrapedNewsModel]
+            A tuple containing ScrapedNewsModels with the scraped data.
         """
         pass
