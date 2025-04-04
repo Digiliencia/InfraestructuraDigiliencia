caec6392beb5f067013a1de05e6119e9cced25aa
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2025-04-04 11:39:02 +0200

    feat: add DatabaseConfig class to handle the database configuration

---
+++ digiliencia/configs/db_config.py
@@ -0,0 +1,35 @@
+from digiliencia.utils.env_loader import EnvLoader
+from digiliencia.exc.invalid_database_config_exc import InvalidDatabaseConfigError
+
+class DatabaseConfig:
+    """Class that holds the database configuration values.
+    
+    Raises:
+        InvalidDatabaseConfigError: If the database configuration variables are not valid.
+    """
+    
+    def __init__(self) -> None:
+        env = EnvLoader()
+        
+        self._uri = env.ddbb_uri
+        self._username = env.ddbb_username
+        self._password = env.ddbb_passwd
+        
+        if not self._is_valid():
+            raise InvalidDatabaseConfigError("Invalid database configuration.")
+        
+    @property
+    def server(self) -> str:
+        return self._uri
+    
+    @property
+    def username(self) -> str:
+        return self._username
+    
+    @property
+    def password(self) -> str:
+        return self._password
+    
+    def _is_valid(self) -> bool:
+        """Checks if the database configuration variables are valid."""
+        return all([self._uri, self._username, self._password])
