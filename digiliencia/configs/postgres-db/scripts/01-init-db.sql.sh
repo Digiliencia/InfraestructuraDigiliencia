0b3193f63de8de0bc473bf843de1347e26316a00
anchueca <achuer00@estudiantes.unileon.es>
2025-05-20 11:58:25 +0200

    feature new user and grant permissions

--- digiliencia/configs/postgres-db/scripts/01-init-db.sql.sh
+++ digiliencia/configs/postgres-db/scripts/01-init-db.sql.sh
@@ -4,7 +4,7 @@ set -e # If somethinf goes wrong, exit immedietely
 echo "Processing SQL template and initializing database/users..."
 
 # Sets the enviroment variables for the database
-VARIABLES_TO_SUBSTITUTE='$APP_DB_NAME $DB_OWNER_USER $DB_OWNER_PASSWORD $APP_USER $APP_USER_PASSWORD'
+VARIABLES_TO_SUBSTITUTE='$APP_DB_NAME $DB_OWNER_USER $DB_OWNER_PASSWORD $APP_USER $APP_USER_PASSWORD $APP_USER_LOGIN $APP_USER_LOGIN_PASSWORD'
 
 # Changes the varialbes to the values in the .env file. Next, it will execute the sql.
 # ON_ERROR_STOP=1 hará que psql salga inmediatamente si encuentra un error
