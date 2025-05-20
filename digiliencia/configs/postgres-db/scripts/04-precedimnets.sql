8556d3cf446ac0828215081eb9b8d46d1f8ccf55
anchueca <achuer00@estudiantes.unileon.es>
2025-05-20 12:22:07 +0200

    features views and procediments

---
+++ digiliencia/configs/postgres-db/scripts/04-precedimnets.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION get_user_id_by_email(user_email VARCHAR)
+RETURNS INTEGER AS $$
+DECLARE
+    user_id INTEGER;
+BEGIN
+    SELECT ID INTO user_id
+    FROM users_id_email_view
+    WHERE email = user_email;
+
+    IF user_id IS NULL THEN
+        RAISE EXCEPTION 'No user found with email %', user_email;
+    END IF;
+
+    RETURN user_id;
+END;
+$$ LANGUAGE plpgsql;
