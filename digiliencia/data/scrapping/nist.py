48632242362c9a646c9dd0341c3feca0c609a5ca
carlos-prieto77 <cpriev00@estudiantes.unileon.es>
2025-05-26 19:08:48 +0200

    feat(nist): function  _is_disabled_button_next

--- digiliencia/data/scrapping/nist.py
+++ digiliencia/data/scrapping/nist.py
@@ -9,12 +9,18 @@ Web scrapping: https://www.nist.gov/nice/ccw-events
 from digiliencia.data.scrapping.abc_scraper import AbstractScraper
 from digiliencia.data.models.events_model import ScrapedEventsModel
 from digiliencia.utils.scrap import ScrapUtils
+from loguru import logger
+from selenium.webdriver.common.by import By
 
 class Nist(AbstractScraper):
-    def __init__(self):
-        self.driver = ScrapUtils.get_driver()
+    '''Scraps only the events of NIST'''
 
-    def _button_more_info(self, pos:int = 0) -> list[str, str]:
+    def __init__(self):
+        logger.debug("Initializing NIST")
+        self.driver = ScrapUtils.get_driver()
+        self.url_cybersegurity = "https://www.nist.gov/nice/ccw-events"
+
+    def _button_more_info(self, pos:int = 0) -> dict[str, str]:
         '''
         The method extract description and url of event. Each event have a button of more info. 
 
@@ -33,16 +39,45 @@ class Nist(AbstractScraper):
         # En caso de no ser de la pagina NIST. Entonces:
         #   Extraer directamente el contenido de la pagina y asignar la url del sitio web
 
-        return ['', '']
+        return {}
 
-    def scrap_events(self, from_days_ago: int) -> list[ScrapedEventsModel]:
+    def _is_disabled_button_next(self):
+        '''
+        Return:
+            True: button next is disabled.
+            False: button next is not disabled.
+        '''
+        disabled_button_next = ".paginate_button.next.disabled"
+        return ScrapUtils.if_element_exists(self.driver, By.CSS_SELECTOR, disabled_button_next) # type: ignore
+
+    def scrap_events_cybersegurity(self, from_days_ago: int = 0) -> list[ScrapedEventsModel]:
         '''
         Access the cybersegurity section of the NIST website and extract information of events.
         
         Args:
-            from_days_ago (int): The number of days to get the information of all events
+            from_days_ago (int): The number of days to get the information of all events by default is 0
 
+        Raise:
+            ValueError: if from_days_ago is less than 0.
+        
         Returns:
             list[ScrapedEventsModel]: all events of website
         '''
-        pass
+        logger.info(f"Getting events from {from_days_ago} days ago")
+
+        if from_days_ago < 1:
+            logger.error("from_days_ago must be greater than 0")
+            raise ValueError("from_days_ago must be greater than 0")
+
+        self.driver.get(self.url_cybersegurity)
+
+        elems_table = self.driver.find_elements(By.CSS_SELECTOR, "tbody tr td")
+        for pos in elems_table:
+            # swicht pos 
+            pass
+
+        news_events: list[ScrapedEventsModel] = []
+
+
+
+        return news_events
