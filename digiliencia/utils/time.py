b6e2c9a23defb5d6922a44c6dd8e4bf8fb1dbbf3
carlos-prieto77 <cpriev00@estudiantes.unileon.es>
2025-06-25 12:06:26 +0200

    feat: enhance date handling in scrap_events and add date list management

--- digiliencia/utils/time.py
+++ digiliencia/utils/time.py
@@ -143,7 +143,7 @@ class TimeUtils:
     @staticmethod
     def days_between_es_dates(date_str1: str, date_str2: str) -> int:
         """
-        Calculates the difference in days between two dates in dd/mm/yyyy format.
+        Calculates the difference in days between two dates in several formats. 
         Args:
             date_str1 (str): The first date string (e.g., "1/10/2023").
             date_str2 (str): The second date string (e.g., "10/10/2023").
@@ -155,7 +155,7 @@ class TimeUtils:
             >>> TimeUtils.days_between_dates('1/10/2023', '10/10/2023')
             9
         """
-        date_formats = ["%d/%m/%Y", "%d %B %Y", "%d %b %Y"]
+        date_formats = ["%d/%m/%Y", "%d %B %Y", "%d %b %Y", "%m/%d/%Y"]
         for date_format in date_formats:
             try:
                 date1 = datetime.strptime(date_str1, date_format)
