d459cab41f3184a2e28237c504446447245f658d
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2025-07-07 16:50:58 +0200

    refactor: clean up import statements and remove commented code in scrap.py

--- digiliencia/cmd/scrap.py
+++ digiliencia/cmd/scrap.py
@@ -1,12 +1,17 @@
+from typing import List
 from loguru import logger
 
+from digiliencia.configs.env import Env
+from digiliencia.data.models.news_model import ScrapedNews
+from digiliencia.data.scrapping.incibe import IncibeScraper
+from digiliencia.data.scrapping.ncsc import Ncsc
 from digiliencia.data.scrapping.weforum import WEForumScraper
+from digiliencia.data.services.neomodel.news_service import NewsService
 
 
 def scrap(from_days_ago: int = 365):
     logger.info("Start scraping")
-    WEForumScraper().scrap_news(from_days_ago)
-    """
+    
     Env()
     news_service = NewsService()
     scrapers = [WEForumScraper, IncibeScraper, Ncsc]
@@ -30,7 +35,7 @@ def scrap(from_days_ago: int = 365):
                     logger.error(f"Error creating news: {create_error}")
         except Exception as e:
             logger.error(f"Error scraping with {scraper.__class__.__name__}: {e}")
-    """
+    
     logger.info("Scraping finished")
 
 
