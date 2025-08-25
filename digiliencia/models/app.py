266de38cfdc24074342b8d87344af9e4af1e2d0c
anchueca <achuer00@estudiantes.unileon.es>
2025-08-25 11:35:38 +0200

    fix: app.py formated

--- digiliencia/models/app.py
+++ digiliencia/models/app.py
@@ -4,14 +4,17 @@ from pydantic import BaseModel
 from typing import List
 from embedding_manager import EmbeddingManager
 
+
 # Define a class for the request
 class TextRequest(BaseModel):
     text: str
+
 
 # Initialize FastAPI
 app = FastAPI()
 
 embedding_manager = EmbeddingManager()
+
 
 # Endpoint
 @app.post("/generate_embedding/")
