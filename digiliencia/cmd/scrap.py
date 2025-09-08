cbf55cc687ebddec775bdf4a030dc0b55efe2c70
carlos-prieto77 <cpriev00@estudiantes.unileon.es>
2025-09-08 19:51:55 +0200

    refactor: remove duplicate import statement for Env in scrap.py

--- digiliencia/cmd/scrap.py
+++ digiliencia/cmd/scrap.py
@@ -1,7 +1,6 @@
 from typing import List
 from configs.env import Env
 from loguru import logger
-from digiliencia.configs.env import Env
 from digiliencia.data.models.news_model import ScrapedNews
 # from configs.env import Env
 from digiliencia.data.scrapping.cyber_canadian import CanadianScraper
