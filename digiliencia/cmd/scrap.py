8a4fcaba87dacf82df252b539b214042630269da
carlos-prieto77 <cpriev00@estudiantes.unileon.es>
2025-09-08 20:07:00 +0200

    refactor: clean up imports and improve code formatting in AmericaCyberAgencyScraper

--- digiliencia/cmd/scrap.py
+++ digiliencia/cmd/scrap.py
@@ -20,7 +20,6 @@ from digiliencia.data.services.neomodel.topic.topic_classification_service impor
     TopicClassificationService
 from digiliencia.exc.dao_create_exc import DAOCreateError
 
-
 def scrap(from_days_ago: int = 5):
     logger.info("Start scraping")
 
@@ -29,7 +28,13 @@ def scrap(from_days_ago: int = 5):
     topics_class_service = TopicClassificationService()
     fields_class_service = FieldClassificationService()
 
-    scrapers = [AmericaCyberAgencyScraper, CanadianScraper, WEForumScraper, IncibeScraper, Ncsc]
+    scrapers = [
+        AmericaCyberAgencyScraper,
+        CanadianScraper,
+        WEForumScraper,
+        IncibeScraper,
+        Ncsc,
+    ]
     for scraper in scrapers:
         try:
             scraped_news: List[ScrapedNews] = scraper().scrap_news(from_days_ago)
