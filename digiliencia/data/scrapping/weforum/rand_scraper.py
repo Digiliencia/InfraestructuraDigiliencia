0a2bd97b0a278da4d50601955794172474b932cb
carlos-prieto77 <cpriev00@estudiantes.unileon.es>
2025-10-20 23:02:19 +0200

    fix(rand_scraper): improve date parsing logic and enhance URL validation

--- digiliencia/data/scrapping/weforum/rand_scraper.py
+++ digiliencia/data/scrapping/weforum/rand_scraper.py
@@ -1,25 +1,13 @@
 import time
 from datetime import datetime
-
 from loguru import logger
 from pydantic import HttpUrl
 from selenium.webdriver.common.by import By
-
 from digiliencia.data.models.news_model import ScrapedNews
 from digiliencia.exc.WEForum_exc import WEForumError
 from digiliencia.utils.scrap import ScrapUtils
-
+from digiliencia.utils.time import TimeUtils
 from .abc_news_scraper import AbstractNewsScraper
-
-'''
-Error scraping https://www.rand.org/pubs/perspectives/PEA4261-1.html:
- Attempted to scrape invalid page for Rand Corporation newsletter scrapper
-'''
-
-'''
-2025-10-20 09:28:14.120 | ERROR    | digiliencia.data.scrapping.weforum.__main__:scrap_news:565 - Error scraping https://www.rand.org/pubs/research_reports/RRA4159-1.html:
- time data '5' does not match format '%b %d, %Y'
-'''
 
 class RandScraper(AbstractNewsScraper):
     def scrap(self, url: str) -> ScrapedNews:
@@ -36,10 +24,9 @@ class RandScraper(AbstractNewsScraper):
         Return:
             ScrapedNews: An object with the publication information.
         """
-        # Verify URL  # TODO fix: 'list' object has no attribute 'text'
         if (
-            "https://www.rand.org/pubs/research_reports/" not in url
-        ):  # TODO fix: Error scraping https://www.rand.org/pubs/perspectives/PEA3886-1.html: Attempted to scrape invalid page for Rand Corporation newsletter scrapper
+            "https://www.rand.org" not in url
+        ):  
             raise WEForumError(
                 "Attempted to scrape invalid page for Rand Corporation newsletter scrapper"
             )
@@ -51,14 +38,15 @@ class RandScraper(AbstractNewsScraper):
 
         title = self.driver.find_element(By.ID, "RANDTitleHeadingId").text
         authors = self.driver.find_element(By.CLASS_NAME, "authors").text
-        date = self.driver.find_element(
+
+        time_elem = self.driver.find_element(
             By.CLASS_NAME, "published"
         ).text  # Mirar el formato de la fecha
-        date = date[10:]
-        date = date[10:]
+        time_elem.replace("Published ", "")
+        date_ft = datetime.strptime(time_elem, TimeUtils().detect_fomat_date(time_elem))  # type: ignore
 
         if ScrapUtils.if_element_exists(
-            self.driver, By.CLASS_NAME, "abstract-first-letter"
+            self.driver, By.CLASS_NAME, "abstract-first-letter" # type: ignore
         ):  # type: ignore
             introduction = self.driver.find_element(
                 By.CLASS_NAME, "abstract-first-letter"
@@ -71,9 +59,7 @@ class RandScraper(AbstractNewsScraper):
 
         return ScrapedNews(
             header=title,
-            date=datetime.strptime(
-                date, "%b %d, %Y"
-            ),  # ERROR se cambio la B en mayuscula por una b en minuscula
+            date=date_ft,
             source="Rand Corporation",
             content=content,
             url=HttpUrl(url),
