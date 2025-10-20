b7084f2cec7e51a7b694d12f410139c6241d7866
carlos-prieto77 <cpriev00@estudiantes.unileon.es>
2025-10-20 22:50:13 +0200

    fix(harvard_business_review_scraper): update date parsing to use dynamic format detection and improve topic selector

--- digiliencia/data/scrapping/weforum/harvard_business_review_scraper.py
+++ digiliencia/data/scrapping/weforum/harvard_business_review_scraper.py
@@ -1,23 +1,13 @@
 import time
 from datetime import datetime
-
 from loguru import logger
 from pydantic import HttpUrl
 from selenium.webdriver.common.by import By
-
 from digiliencia.data.models.news_model import ScrapedNews
 from digiliencia.exc.WEForum_exc import WEForumError
 from digiliencia.utils.scrap import ScrapUtils
-
+from digiliencia.utils.time import TimeUtils
 from .abc_news_scraper import AbstractNewsScraper
-
-'''
-2025-10-20 09:24:32.431 | ERROR    | digiliencia.data.scrapping.weforum.__main__:scrap_news:565 - Error scraping https://hbr.org/sponsored/2025/10/the-business-rewards-and-identity-risks-of-agentic-ai:   
- Message: invalid selector: No selector specified
-  (Session info: chrome=141.0.7390.108); For documentation on this error, please visit: https://www.selenium.dev/documentation/webdriver/troubleshooting/errors#invalidselectorexception
-Stacktrace:
-        GetHandleVerifier [0x0xe3fe83+66515]
-'''
 
 class HarvardBusinessReviewScraper(AbstractNewsScraper):
     def scrap(self, url: str) -> ScrapedNews:
@@ -48,7 +38,7 @@ class HarvardBusinessReviewScraper(AbstractNewsScraper):
                 elems["title"] = "h1[class=sponsored-article-hed]"
                 elems["date"] = ".publication-date"
                 elems["content"] = "content p"
-                elems["topic"] = ""  # There is not topic
+                elems["topic"] = "a strong em"
             else:
                 elems["title"] = "div.Title_standard__x_GEq.Title_standard__x_GEq"
                 elems["date"] = (
@@ -71,10 +61,9 @@ class HarvardBusinessReviewScraper(AbstractNewsScraper):
 
         title = self.driver.find_element(By.CSS_SELECTOR, elems["title"]).text
 
-        # TODO hacer metodo para saber si el mes tendría que ser b o B
         time_elem = self.driver.find_element(By.CSS_SELECTOR, elems["date"]).text
         date_ft = time_elem.replace(",", "")
-        date = datetime.strptime(date_ft, "%B %d %Y")  # type: ignore # ERROR se ha cambiado la b minuscula a una B en mayuscula
+        date = datetime.strptime(date_ft, TimeUtils().detect_fomat_date(time_elem))  # type: ignore 
 
         content_container = self.driver.find_elements(By.CSS_SELECTOR, elems["content"])
         content = [contents.text for contents in content_container]
