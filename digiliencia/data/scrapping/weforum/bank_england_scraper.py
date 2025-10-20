3b0c8ecef9466f670c640c87db4602b30a7e06cd
carlos-prieto77 <cpriev00@estudiantes.unileon.es>
2025-10-20 22:37:40 +0200

    refactor(bank_england_scraper): enhance element selection logic and improve error handling

--- digiliencia/data/scrapping/weforum/bank_england_scraper.py
+++ digiliencia/data/scrapping/weforum/bank_england_scraper.py
@@ -1,38 +1,13 @@
 import time
 from datetime import datetime
-
 from loguru import logger
 from pydantic import HttpUrl
 from selenium.webdriver.common.by import By
-
 from digiliencia.data.models.news_model import ScrapedNews
 from digiliencia.exc.WEForum_exc import WEForumError
-
+from digiliencia.utils.scrap import ScrapUtils
+from digiliencia.utils.time import TimeUtils
 from .abc_news_scraper import AbstractNewsScraper
-
-'''
-Error scraping https://www.bankofengland.co.uk/report/2025/the-boes-approach-to-innovation-in-ai-dlt-quantum-computing:
- Attempted to scrape invalid page for Bank of England article scrapper
-'''
-
-'''
-2025-10-20 09:21:57.153 | ERROR    | digiliencia.data.scrapping.weforum.__main__:scrap_news:565 - Error scraping https://blogs.lse.ac.uk/europpblog/2025/10/14/attention-is-all-you-need-europe-should-decouple-from-us-technology-eurostack/:
- Message: no such element: Unable to locate element: {"method":"css selector","selector":"div.mobile-post-main-image__date > h3:nth-of-type(4)"}
-  (Session info: chrome=141.0.7390.108); For documentation on this error, please visit: https://www.selenium.dev/documentation/webdriver/troubleshooting/errors#nosuchelementexception
-Stacktrace:
-        GetHandleVerifier [0x0xe3fe83+66515]
-        GetHandleVerifier [0x0xe3fec4+66580]
-        (No symbol) [0x0xc2dc48]
-        (No symbol) [0x0xc78704]
-'''
-
-'''
-2025-10-20 09:40:06.248 | ERROR    | digiliencia.data.scrapping.weforum.__main__:scrap_news:565 - Error scraping https://blogs.lse.ac.uk/usappblog/2025/09/19/geopolitics-isnt-killing-global-supply-chains-its-powering-them/:
- Message: no such element: Unable to locate element: {"method":"css selector","selector":"div.mobile-post-main-image__date > h3:nth-of-type(4)"}
-  (Session info: chrome=141.0.7390.108); For documentation on this error, please visit: https://www.selenium.dev/documentation/webdriver/troubleshooting/errors#nosuchelementexception
-Stacktrace:
-        GetHandleVerifier [0x0xe3fe83+66515]
-'''
 
 class BankEnglandScraper(AbstractNewsScraper):
     def scrap(self, url: str) -> ScrapedNews:
@@ -51,7 +26,7 @@ class BankEnglandScraper(AbstractNewsScraper):
         """
         url = url
         logger.debug(f"Scraping Bank of England article: {url}")
-        if "https://blogs.lse.ac.uk/" not in url:
+        if ("https://blogs.lse.ac.uk/" not in url) or ("https://www.bankofengland.co.uk/" not in url):
             raise WEForumError(
                 "Attempted to scrape invalid page for Bank of England article scrapper"
             )
@@ -59,17 +34,41 @@ class BankEnglandScraper(AbstractNewsScraper):
         self.driver.get(url)
         time.sleep(self.load_time)  # Reject cookies if visible
 
-        title = self.driver.find_element(By.CSS_SELECTOR, "h1[itemprop='name']").text
+        if ScrapUtils().if_element_exists(self.driver, By.CSS_SELECTOR, "h1[itemprop='name']"): # type: ignore
+            title = self.driver.find_element(By.CSS_SELECTOR, "h1[itemprop='name']").text
+        elif ScrapUtils().if_element_exists(self.driver, By.CSS_SELECTOR, "h1[itemprop='name']"): # type: ignore
+            title = self.driver.find_element(By.CSS_SELECTOR, "h1[itemprop='name']").text
+        else:
+            title = self.driver.find_element(By.CSS_SELECTOR, ".mobile-post-main-image__date + h1").text
 
-        time_elem = self.driver.find_element(By.CLASS_NAME, "published-date").text
+        if ScrapUtils().if_element_exists(self.driver, By.CSS_SELECTOR, ".mobile-post-main-image__date h3"): # type: ignore
+            time_elem = self.driver.find_elements(By.CSS_SELECTOR, ".mobile-post-main-image__date h3")[1].text
+        elif ScrapUtils().if_element_exists(By.CSS_SELECTOR, ".published-date"): # type: ignore
+            time_elem = self.driver.find_element(By.CSS_SELECTOR, ".published-date").text
+        else:
+            time_elem = self.driver.find_element(By.CLASS_NAME, "published-date").text
+
+        fmt_date = TimeUtils().detect_fomat_date(time_elem)
         date_ft = time_elem.replace("Published on ", "")
-        date = datetime.strptime(date_ft, "%B %d, %Y")  # type: ignore
+        date = datetime.strptime(date_ft, fmt_date)  # type: ignore
 
-        author = "Bank of England"  # There is not author
+        if ScrapUtils().if_element_exists(self.driver, By.CSS_SELECTOR, ".mobile-post-main-image__date h3"): # type: ignore
+            author = self.driver.find_elements(By.CSS_SELECTOR, ".mobile-post-main-image__date h3")[0].text
+        else:
+            author = "Bank of England"  # There is not author
 
-        content_container = self.driver.find_elements(
-            By.CSS_SELECTOR, "div[id='output'] p"
-        )
+        if ScrapUtils().if_element_exists(self.driver, By.CSS_SELECTOR, "div[id='output'] p"): # type: ignore
+            content_container = self.driver.find_elements(
+                By.CSS_SELECTOR, "div[id='output'] p"
+            )
+        elif ScrapUtils().if_element_exists(self.driver, By.CSS_SELECTOR, ".page-section p"): # type: ignore
+            content_container = self.driver.find_elements(
+                By.CSS_SELECTOR, ".page-section p"
+            )
+        else:
+            content_container = self.driver.find_elements(
+                By.CSS_SELECTOR, ".post-content p"
+            )
         content = [contents.text for contents in content_container]
         content = "".join(content)
 
