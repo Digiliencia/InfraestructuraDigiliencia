5b3f2f24a549cdaaf0f69a4d5d84e360369ccea9
anchueca <achur@unileon.es>
2026-01-07 15:38:44 +0100

    fix: update HOST constant in conftest.py to use fastapi-service-test for testing environment

--- test/fastApi_test/conftest.py
+++ test/fastApi_test/conftest.py
@@ -32,7 +32,7 @@ from digiliencia.configs.fastAPI.core.endpoints import (
 from test.conftest import faker
 
 # Constants
-HOST = "127.0.0.1"
+HOST = "fastapi-service-test"
 PORT = "8080"
 BASE_URL = f"http://{HOST}:{PORT}/api"
 HEALTH_CHECK_URL = f"{BASE_URL}/health"
